7d5f0d98708631d064417d8a4ffe6b30a1c521c1
Hakan DİNÇTÜRK <hakan.dincturkk@gmail.com>
2022-05-27 15:02:40 +0300

    models

---
+++ api/src/migrations/20220527115505-create-users.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable('Users', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			username: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			email: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			password: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			name: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			surname: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			isDeleted: {
+				type: Sequelize.BOOLEAN,
+				allowNull: false
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				default: Date.now
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				default: Date.now
+			}
+		});
+	},
+	// eslint-disable-next-line no-unused-vars
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('Users');
+	}
+};
