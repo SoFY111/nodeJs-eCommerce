535500dc9c0b6928c6b41cf2acc9e4dcda11b985
Hakan DİNÇTÜRK <58170470+SoFY111@users.noreply.github.com>
2022-08-03 10:37:39 +0300

    jenkins update 1

--- api/Private/middlewares/checkauth.js
+++ api/Private/middlewares/checkauth.js
@@ -11,6 +11,6 @@ module.exports = async (req, res, next) => {
 	}
 	catch (error) {
 		console.log(error.message);
-		res.json({type: false, message: 'unauthorized'});
+		res.json({type: false, message: 'unauthorizedd'});
 	}
 };
